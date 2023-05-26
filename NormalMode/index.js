1986171903c9780749d8bdb89a5840524cf71ac3
Avishek Majumder <avishekmajumder2004@gmail.com>
2023-05-26 23:27:13 +0530

    Added some css and rotation of player and bullet

--- NormalMode/index.js
+++ NormalMode/index.js
@@ -5,17 +5,17 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 
-let player_width = 40
-let player_height = 40
-let homebase_width = 100
-let homebase_height = 30
+let player_width = 60
+let player_height = 60
+let homebase_width = canvas.width*0.8
+let homebase_height = canvas.height*0.15
 
 
 class Player{
     constructor(){
         this.width = player_width
         this.height = player_height
-        this.max = 5
+        this.max = 15
         this.attackerpool = []
         this.base = new Base();
         this.health = new HealthBar(10,10,150,20,this.base.health)
@@ -28,7 +28,16 @@ class Player{
             x: 0,
             y: 0
         }      
+        const img = new Image()
+        img.src = './Pictures/tank.png'
+        this.image = img
+
+        const bg_img = new Image()
+        bg_img.src = './Pictures/background.png'
+        this.bg_img = bg_img
         
+
+        this.rotation = 0
     }
 
     spawn_attacker(){
@@ -38,10 +47,20 @@ class Player{
     }
 
     draw(){
-        //Drawing the player
         ctx.clearRect(0,0, canvas.width, canvas.height)
-        ctx.fillStyle = "red"
-        ctx.fillRect(this.position.x, this.position.y, this.width,this.height)
+        //Setting background
+        ctx.drawImage(this.bg_img,0,0, canvas.width,canvas.height)
+
+        //Drawing the player        
+        // ctx.fillStyle = "red"
+        // ctx.fillRect(this.position.x, this.position.y, this.width,this.height)
+        ctx.save()
+        ctx.translate(player.position.x + player.width/2, player.position.y + player.height/2)
+        ctx.rotate(player.rotation)
+        ctx.translate(-player.position.x - player.width/2, -player.position.y - player.height/2)
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+        ctx.restore()
+        
         //drawing the attackers
         this.attackerpool.forEach(attacker => {
             attacker.draw(ctx)
@@ -51,11 +70,14 @@ class Player{
         this.base.draw()
         //drawing the healthbar
         Health.update(this.base.health)
-        //Checking
-
-
-        
-
+        //Checking if any wave is finished
+        if(this.attackerpool.length ===0){
+            this.spawn_attacker()
+        }
+        //Cheking if health is done
+        if(player.base.health ===0){
+            cancelAnimationFrame(animation)
+        }
     }
     update(){
         this.position.x += this.velocity.x
@@ -73,7 +95,7 @@ class Attackers {
         this.x = Math.random() * canvas.width * 0.75;
         this.x += 0.25/2*canvas.width
         this.y = Math.random() * canvas.height *0.1;
-        this.speed = 0.5
+        this.speed = 1.3
         this.radius = 20
     }
 
@@ -90,17 +112,26 @@ class Attackers {
 }
 
 class projectile{
-    constructor({position, velocity}){
+    constructor({position, velocity, rotation}){
         this.position = position
         this.velocity = velocity
-        
+        this.rotation = rotation;
         this.radius = 3
+        const img = new Image()
+        img.src = './Pictures/bullet.png'
+        this.image = img
     }
 
     draw(){
+        ctx.save()
+        ctx.translate(this.position.x + this.radius, this.position.y + this.radius)
+        ctx.rotate(this.rotation)
+        ctx.translate(-this.position.x - this.radius, -this.position.y - this.radius)
+        ctx.drawImage(this.image, this.position.x-7, this.position.y-7, 13,13)
+        ctx.restore()
         ctx.beginPath()
         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI *2)
-        ctx.fillStyle = 'orange'
+        ctx.fillStyle = 'transparent'
         ctx.fill()
         ctx.closePath()
     }
@@ -114,6 +145,8 @@ class projectile{
 
 class Base{
     constructor(){
+        this.x= canvas.width*0.1
+        this.y = canvas.height*0.8
         this.width = homebase_width;
         this.height = homebase_height ;
         this.health = 100;
@@ -121,7 +154,7 @@ class Base{
 
     draw(){
         ctx.fillStyle = 'yellow'
-        ctx.fillRect(canvas.width*0.25, canvas.height*0.8, canvas.width*0.5, canvas.height*0.15)
+        ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 
 }
@@ -151,31 +184,27 @@ class HealthBar{
 }
 
 
-
-
 const player = new Player()   //Every class is spawned in player class
 const Health = new HealthBar(10,10,150,20,100)
 const projectiles = []     
 
 
-
+let animation;
 function gameloop(){
-    requestAnimationFrame(gameloop)
+    animation = requestAnimationFrame(gameloop)
     player.update()   //player is drawn
-
+    
+    //Checking if projectile hits Attackers
     for(let i =0 ; i < projectiles.length ; i++){
         let p_x =projectiles[i].position.x
         let p_y = projectiles[i].position.y
-        // console.log(projectiles[i].position.x, projectiles[i].position.y)
         for(let j =0; j < player.attackerpool.length ; j++){
             let a_x = player.attackerpool[j].x
             let a_y = player.attackerpool[j].y
             if( (p_x - a_x)**2 + (p_y - a_y)**2 - player.attackerpool[j].radius**2 < 0){
                 projectiles.splice(i,1)
                 player.attackerpool.splice(j,1)
-                console.log("BOOOM")
             }
-            // console.log(player.attackerpool[j].x, player.attackerpool[j].y)
         }
     }
     
@@ -194,6 +223,15 @@ function gameloop(){
             projectile.update()
         }
     })
+
+    //If attackers hit home base
+    for(let i = 0; i < player.attackerpool.length;i++){
+        //  console.log(Math.round(player.attackerpool[i].y + player.attackerpool[i].radius),Math.round(player.base.y))
+        if(Math.round(player.attackerpool[i].y + player.attackerpool[i].radius) >= Math.round(player.base.y)){
+            player.base.health -= 5
+            player.attackerpool.splice(i,1)
+        }
+    }
 }
 gameloop()   //Runs the game
 
@@ -214,7 +252,6 @@ addEventListener('keydown', ({key}) => {
             break
         case ' ':
             player.base.health -= 10
-
         }
 })
 
@@ -226,6 +263,13 @@ addEventListener("click", function (e) {
     let mod = Math.sqrt( ( m_x -p_x)**2  + (m_y-p_y)**2)
     const scaling_x = (m_x-p_x)/mod
     const scaling_y = (m_y-p_y)/mod
+    let rot = Math.acos((p_y-m_y)/mod)
+    if(m_x - p_x > 0){
+        rot = rot
+    }else{
+        rot = -rot
+    } 
+    
     projectiles.push(new projectile({
         position:{
             x:player.position.x  + player.width /2,
@@ -234,10 +278,27 @@ addEventListener("click", function (e) {
         velocity:{
             x:5 * scaling_x,
             y:5* scaling_y
-        }
+        },
+        rotation : rot
     }))
 })
 
+
+addEventListener("mousemove" , (e) => {
+    // console.log(e.clientX, e.clientY)
+    let m_x = e.clientX
+    let m_y = e.clientY
+    let p_x = player.position.x
+    let p_y = player.position.y
+    let mod = Math.sqrt( ( m_x -p_x)**2  + (m_y-p_y)**2)
+    let rot = Math.acos((p_y-m_y)/mod)
+    
+    if(m_x - p_x > 0){
+        player.rotation = rot
+    }else{
+        player.rotation = -rot
+    }    
+})
 
 
 
